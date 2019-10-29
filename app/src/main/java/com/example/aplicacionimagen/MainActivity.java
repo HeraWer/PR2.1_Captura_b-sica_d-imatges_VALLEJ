94fe3fcdf7d2d5dbe2feff10aac9e0845fce03ff
vallejestudiante <vallejestudiante@gmail.com>
2019-10-29 16:00:14 +0100

    Capturar y mostrar imagen echo

--- app/src/main/java/com/example/aplicacionimagen/MainActivity.java
+++ app/src/main/java/com/example/aplicacionimagen/MainActivity.java
@@ -1,22 +1,22 @@
+
 package com.example.aplicacionimagen;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+        import androidx.annotation.RequiresApi;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.provider.MediaStore;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imageView;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        imageView = findViewById(R.id.imageView);
         Button button = findViewById(R.id.buttonCamera);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,10 +44,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
+
         }
     }
+
+
 }
